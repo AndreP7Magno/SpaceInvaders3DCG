3c1c160ffa5d29aa6e2c0184a4951d8b87ce9f80
AndreP7Magno <andremagno14@gmail.com>
2018-11-21 23:24:29 -0200

    Revert "adicionada a classe para escrever na tela"
    This reverts commit 51c6197cacceae5d8af47408a5492e1111e17ec5.

--- SpaceInvaders3DCG/SpaceInvaders3DCG/escreve.h
+++ SpaceInvaders3DCG/SpaceInvaders3DCG/escreve.h
@@ -10,11 +10,11 @@ class escreve
 public:
 };
 
-void EscrevePontuacaoGeral(int cont) {
-	char texto[12] = "Pontuacao: ";
-	char textoPontuacao[20];
+void Escreve(int cont) {
+	char texto[11] = "Pontuacao:";
+	char teste[20];
 
-	sprintf_s(textoPontuacao, "%d", cont);
+	sprintf_s(teste, "%d", cont);
 
 	glColor3ub(255, 255, 255);
 	glRasterPos3f(0.7, -2.0, 0.0);
@@ -22,89 +22,6 @@ void EscrevePontuacaoGeral(int cont) {
 	for (int k = 0; k <= strlen(texto); k++)
 		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, texto[k]);
 
-	for (int k = 0; k <= strlen(textoPontuacao); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, textoPontuacao[k]);
-}
-
-void EscrevePerdedor() {
-	char texto[11] = "PERDEU";
-
-	glColor3ub(254, 255, 255);
-	glRasterPos3f(-0.5, 1.0, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, texto[k]);
-}
-
-void EscreveGanhador() {
-	char texto[8] = "GANHOU";
-
-	glColor3ub(255, 255, 255);
-	glRasterPos3f(-0.5, 1.0, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, texto[k]);
-}
-
-void EscreveMenuFinal() {
-	char texto[50] = "Você zerou o joguinho";
-
-	glColor3ub(255, 255, 255);
-	glRasterPos3f(-0.5, 1.0, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, texto[k]);
-}
-
-void EscreveMenuAuxiliar() {
-	char texto[50] = "Aperte Z para Reiniciar ou X para Fechar";
-
-	glColor3ub(255, 255, 255);
-	glRasterPos3f(-2.0, 0.7, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, texto[k]);
-}
-
-void EscreveMenuAuxiliar2() {
-	char texto[150] = "Aperte Z para ir ao próximo nível ou X para Fechar";
-
-	glColor3ub(255, 255, 255);
-	glRasterPos3f(-2.0, -0.7, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, texto[k]);
-
-}
-
-void EscreveBV() {
-	char texto[50] = "BEM VINDO AO SPACE INVADERS 3D";
-
-	glColor3ub(255, 255, 255);
-	glRasterPos3f(-0.5, 1.0, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, texto[k]);
-
-}
-
-void EscreveInicio() {
-	char texto[50] = "Aperte Z para Iniciar ou X para Fechar";
-
-	glColor3ub(255, 255, 255);
-	glRasterPos3f(-2.0, -0.7, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, texto[k]);
-
-}
-
-void creditos() {
-	char texto[150] = "André Magno e William Hoffmann";
-
-	glColor3ub(255, 255, 255);
-	glRasterPos3f(-2.5, -2.5, 0.0);
-
-	for (int k = 0; k <= strlen(texto); k++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, texto[k]);
+	for (int k = 0; k <= strlen(teste); k++)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, teste[k]);
 }
